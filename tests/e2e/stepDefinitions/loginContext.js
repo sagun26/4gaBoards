91302660389556ebebe982101fb39104e5dad6b4
sagun26 <sagunsigdel6@gmail.com>
2026-04-21 15:04:08 +0545

    feat:pom implementation

--- tests/e2e/stepDefinitions/loginContext.js
+++ tests/e2e/stepDefinitions/loginContext.js
@@ -1,30 +1,21 @@
 const { Given, When, Then, DataTable } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
-const dashboardSelector = "div[title='Dashboard']";
-const emailSelector = "[name='emailOrUsername']";
-const paasswordSelector = "[name='password']";
-const baseurl = 'http://localhost:3000/';
-const loginurl = 'http://localhost:3000/login';
+const { LoginPage } = require('../pageObjects/LoginPage.js');
+const login = new LoginPage();
 
 Given('admin user has navigated to the login page', async function () {
-  await page.goto(loginurl);
+  await login.navigateToLoginPage();
 
-  expect(page.url()).toBe(loginurl);
+  expect(page.url()).toBe(login.loginUrl);
 });
 
 When('admin user log in with following credentials', async function (dataTable) {
   const loginCredentails = dataTable.hashes();
 
-  await page.locator(emailSelector).fill(loginCredentails[0].email);
-
-  await page.locator(paasswordSelector).fill(loginCredentails[0].password);
-  await page.getByRole('button', { name: 'Log in' }).click();
+  await login.login(loginCredentails);
 });
 
 Then('admin user should be navigated to admin panel dashboard', async function () {
-  await page.goto(baseurl);
-
-  expect(page.url()).toBe(baseurl);
-  await expect(page.locator(dashboardSelector)).toBeVisible();
+  await expect(page.locator(login.dashboardSelector)).toBeVisible();
 });
